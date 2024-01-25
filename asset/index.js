f283f38b47059d401ff8e441b3cfd463a0057944
Asadullah816 <johnbhai608@gmail.com>
2024-01-25 12:52:17 +0500

    header scroll etc

--- asset/index.js
+++ asset/index.js
@@ -1,15 +1,25 @@
-const hero_section= document.querySelector(".hero-section")
-const nav_header=document.querySelector(".header-area");
-const observer= new IntersectionObserver((entries)=>{ 
-    const ent = entries[0];
-    console.log(ent)
-    ent.isIntersecting==false ? nav_header.classList.add("sticky") :
-    nav_header.classList.remove("sticky");
- }, {
-    root:null,
-    rootMargin:"",
-    threshold:"",
-});
+const hero_section = document.querySelector(".hero-section")
+const nav_header = document.querySelector(".header-area");
+// const observer = new IntersectionObserver((entries) => {
+//     const ent = entries[0];
+//     console.log(ent)
+
+//     ent.isIntersecting == false ? nav_header.classList.add("sticky") :
+//         nav_header.classList.remove("sticky");
+// }, {
+//     root: null,
+//     rootMargin: "",
+//     threshold: "",
+// });
+window.addEventListener('scroll', function () {
+    if (window.scrollY > hero_section.offsetTop) {
+        nav_header.classList.add("sticky");
+    }
+    else {
+        nav_header.classList.remove("sticky")
+    }
+
+})
 
 observer.observe(hero_section);
 
