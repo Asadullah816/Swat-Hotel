700548b3978db346ef4229287d0de524f88b2a88
Asadullah816 <johnbhai608@gmail.com>
2024-06-11 18:46:30 +0500

    room section

--- asset/index.js
+++ asset/index.js
@@ -23,3 +23,32 @@ window.addEventListener('scroll', function () {
 
 // observer.observe(hero_section);
 
+let img = document.querySelectorAll('.nroom-img');
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev')
+let pimg = document.querySelector('.room-image');
+index = 0;
+next.addEventListener('click', function () {
+    img.forEach(function (e) {
+        e.classList.remove('show')
+    })
+    if (index < img.length - 1) {
+        index++;
+        img[index].classList.add('show');
+    } else {
+        index = 0;
+        img[index].classList.add('show');
+    }
+})
+prev.addEventListener('click', function () {
+    img.forEach(function (e) {
+        e.classList.remove('show')
+    })
+    if (index > 0) {
+        index--;
+        img[index].classList.add('show');
+    } else {
+        index = img.length - 1;
+        img[index].classList.add('show');
+    }
+})
